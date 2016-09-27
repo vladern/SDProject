10b6e8c5c16ad8815c39077ae1b0d3791d4b8e5a
vladernn <vladys.3234@gmail.com>
2016-09-27 11:10:58 +0200

    Commit del principio v0.01

---
+++ MyHTTPServer/MyHTTPServer.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.net.*;
+
+public class MyHTTPServer 
+{
+
+	public static void main(String[] args)
+	{
+		// creación del socket
+		int port = 8080;
+		try
+		{
+
+			ServerSocket serverSocket = new ServerSocket(port);
+			System.err.println("El servidor corre con el puerto: "+ port);
+
+			// espera las conexiones de clientes 
+			while(true)
+			{
+				// se bloquea en este punto hasta que reciba una conexión
+				Socket clientSocket = serverSocket.accept();
+				System.err.println("Un nuevo cliente se ha conectado");
+
+				// flujo de conversación 
+				BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+				BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+
+				String s;
+				s = in.readLine();
+				while(s != null)
+				{
+				System.out.println(s);
+
+					if(s.isEmpty())
+					{
+						break;
+					}
+					break;
+				}
+
+				out.write("HTTP/1.0 200 OK\r\n");
+				// Header...
+				out.write("Last-modified: Fri, 09 Aug 1996 14:21:40 GMT\r\n");
+				out.write("\r\n"); // The content starts afters this empty line
+				out.write("<TITLE>Hello!</TITLE>\r\n");
+				out.write("<body><h1>Hola mundo</h1></body>");
+
+				//al ternimar el flujo
+				System.err.println("Conexión ha terminado");
+				out.close();
+				in.close();
+				clientSocket.close();
+			}
+		}catch(IOException e){};
+	}
+}
