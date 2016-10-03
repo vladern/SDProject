7910d01072c32c78c99ac7d54ee14e227e5578d5
vladernn <vladys.3234@gmail.com>
2016-10-03 12:53:46 +0200

    servidor concurente

--- MyHTTPServer/MyHTTPServer.java
+++ MyHTTPServer/MyHTTPServer.java
@@ -19,11 +19,33 @@ public class MyHTTPServer
 	}
 	public void DefaultConection()
 	{
+			int cola = 4;
+			int port = 8080;
+			try
+			{
+				System.out.println("El servidor corre con el puerto: "+ port);
+				// se bloquea en este punto hasta que reciba una conexión
+				ServerSocket serverSocket = new ServerSocket(port);
+				while(true)
+				{
+					Socket clientSocket = serverSocket.accept();
+					Thread hilo0 = new Proceso(clientSocket);
+					hilo0.start();
+					System.out.println("Un nuevo cliente se ha conectado");
+				}
+			}catch(IOException e){}
 
-			 Thread hilo = new Proceso("proceso 1");
-			 Thread hilo2 = new Proceso("proceso 2");
+	}
 
-			 hilo.start();
-			 hilo2.start();
+	public String leerArchivo(String archivo) throws FileNotFoundException, IOException 
+	{
+	      String cadena ="";
+	      FileReader f = new FileReader(archivo);
+	      BufferedReader b = new BufferedReader(f);
+	      while(b.readLine()!=null) {
+	          cadena = cadena + b.readLine();
+	      }
+	      b.close();
+	      return cadena;
 	}
 }
--- MyHTTPServer/Proceso.java
+++ MyHTTPServer/Proceso.java
@@ -4,56 +4,45 @@ import java.net.*;
 
 public class Proceso extends Thread
 {
-	public Proceso(String msg)
+	private Socket skCliente;		
+	public Proceso(Socket psCliente)
 	{
-		super(msg);
+		this.skCliente = psCliente;
 	}
 	public void run()
 	{
-		int port = 8080;
-		System.out.println("Se esta ejecutando: " + this.getName());
-		
-		System.err.println("El servidor corre con el puerto: "+ port);
 		try
 		{
-		ServerSocket serverSocket = new ServerSocket(port);
+
 			while(true)
 			{
-				// se bloquea en este punto hasta que reciba una conexión
-				Socket clientSocket = serverSocket.accept();
-				System.err.println("Un nuevo cliente se ha conectado");
-
 				// flujo de conversación 
-				BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-				BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-
+				BufferedReader in = new BufferedReader(new InputStreamReader(this.skCliente.getInputStream()));
+				BufferedWriter out = new BufferedWriter(new OutputStreamWriter(this.skCliente.getOutputStream()));
 				String s;
 				s = in.readLine();
-				while(s != null)
-				{
-				System.out.println(s);
+				System.out.println("Estoy dentro!");
 
-					if(s.isEmpty())
-					{
-						break;
-					}
-					break;
-				}
-		
-				out.write("HTTP/1.0 200 OK\r\n");
+				MyHTTPServer archivoHTML = new MyHTTPServer();
+				System.out.println("antes de leer el archivo");
+				String archivo = archivoHTML.leerArchivo("/home/vladernn/Escritorio/sd.txt");
+				System.out.println("despues de leer el archivo");
+				System.out.println(archivo);
+				out.write("");
+			/*	out.write("HTTP/1.0 200 OK\r\n");
 				// Header...
 				out.write("Last-modified: Fri, 09 Aug 2016 14:21:40 GMT\r\n");
 				out.write("\r\n"); // The content starts afters this empty line
 				out.write("<TITLE>GACCLM</TITLE>\r\n");
-
+			
 				MyHTTPServer server = new MyHTTPServer();
 				out.write(server.DefaultPage());
-
+			*/
 				//al ternimar el flujo
 				System.err.println("Conexión ha terminado");
 				out.close();
 				in.close();
-				clientSocket.close();
+				this.skCliente.close();
 			}
 		}catch(IOException e){};
 	}
