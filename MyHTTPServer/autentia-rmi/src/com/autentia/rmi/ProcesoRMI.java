5b64c12b2d5a3a276abcda1d3e30bb1ed6ee2d1c
vladernn <vladys.3234@gmail.com>
2016-10-22 16:29:38 +0200

    prueba del Controler

--- MyHTTPServer/autentia-rmi/src/com/autentia/rmi/ProcesoRMI.java
+++ MyHTTPServer/autentia-rmi/src/com/autentia/rmi/ProcesoRMI.java
@@ -1,7 +1,9 @@
 package com.autentia.rmi;
 import java.io.*;
 import java.net.*;
-
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.NotBoundException;
 public class ProcesoRMI extends Thread 
 {
 	private String remoteHost;
@@ -19,8 +21,24 @@ public class ProcesoRMI extends Thread
 			BufferedReader in = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream())); //buffer de entrada
 			BufferedWriter out = new BufferedWriter(new OutputStreamWriter(this.clientSocket.getOutputStream()));//buffer de salida
 			String peticion = in.readLine();
+			String buscado = "setluz";
 			System.out.println("++"+peticion+"++");
-		/*	Registry registry = LocateRegistry.getRegistry(remoteHost, Registry.REGISTRY_PORT);
+			boolean encontradoSet = peticion.toUpperCase().contains(buscado.toUpperCase()); // busco si contiene setluz
+			if(encontradoSet)
+			{
+				String[] partes = peticion.split("?");
+				String[] apartado = partes[0].split("=");
+				String[] direccion = partes[1].split("=");
+				System.out.println("apartado:"+apartado[1]+"direccion:"+direccion[1]);
+			}else
+			{
+				String[] partes = peticion.split("?");
+				String apartado = partes[0];
+				String[] direccion = partes[1].split("=");
+				System.out.println("apartado:"+apartado+"direccion:"+direccion[1]);
+			}
+
+			Registry registry = LocateRegistry.getRegistry(remoteHost, Registry.REGISTRY_PORT);
 			String[] remoteObjNames = registry.list();
 			for (String remoteObjName : remoteObjNames)
 			{
@@ -32,8 +50,10 @@ public class ProcesoRMI extends Thread
 					System.out.println(server.sayHelloWorld());
 				}
 			}
-		*/
 		}catch(IOException e)
+		{
+
+		}catch(NotBoundException ex)
 		{
 
 		}
